1d2089cc1a958b7d317cae9e0f228647b4c67c8f
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-27 23:24:26 +0900

    Add algorithm solving 21.11.27

--- November/211127.js
+++ November/211127.js
@@ -10,3 +10,17 @@ function rowSumOddNumbers(n) {
 
 console.log(rowSumOddNumbers(1))
 console.log(rowSumOddNumbers(4))
+
+
+function validatePIN (pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
+
+
+console.log(validatePIN("1234"))
+console.log(validatePIN("12345"))
+console.log(validatePIN("a234"))
+console.log(validatePIN("1.234"))
+console.log(validatePIN("12.0"))
+console.log(validatePIN("82771g"))
+console.log(validatePIN("0000"))
