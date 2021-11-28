ba1c2ae5fbc577953e4a57eab1c12f49f3a17490
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-28 17:36:58 +0900

    Add algorithm solving 21.11.28

--- November/211128.js
+++ November/211128.js
@@ -9,3 +9,23 @@ function findNextSquare(sq) {
 console.log(findNextSquare(121))
 console.log(findNextSquare(625))
 console.log(findNextSquare(114))
+
+
+function addBinary(a, b) {
+  return Math.abs(a+b).toString(2)
+}
+
+console.log(addBinary(1, 2))
+console.log(addBinary(1, 1))
+console.log(addBinary(5, 9))
+
+
+function printerError(s) {
+  // let arr = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  // let count = s.split("").filter((el) => arr.includes(el))
+  // return `${count.length}/${s.length}`
+  return `${(s.match(/[n-z]/g) || []).length}/${s.length}`;
+}
+
+console.log(printerError("aaabbbbhaijjjm"))
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"))
