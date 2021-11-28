e378296ccf9be9c7730d3a8e79755b2039c1bd67
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-28 17:48:14 +0900

    Add algorithm solving 21.11.28

--- November/211128.js
+++ November/211128.js
@@ -11,6 +11,7 @@ console.log(findNextSquare(625))
 console.log(findNextSquare(114))
 
 
+
 function addBinary(a, b) {
   return Math.abs(a+b).toString(2)
 }
@@ -18,6 +19,7 @@ function addBinary(a, b) {
 console.log(addBinary(1, 2))
 console.log(addBinary(1, 1))
 console.log(addBinary(5, 9))
+
 
 
 function printerError(s) {
@@ -29,3 +31,21 @@ function printerError(s) {
 
 console.log(printerError("aaabbbbhaijjjm"))
 console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"))
+
+
+
+function openOrSenior(data) {
+  // let result = []
+  // for(let i = 0; i < data.length; i++) {
+  //   if(data[i][0] >= 55 && data[i][1] > 7) {
+  //     result.push('Senior')
+  //   } else {
+  //     result.push('Open')
+  //   }
+  // }
+  // return result
+  return data.map((el) => el[0] >= 55 && el[1] > 7 ? 'Senior' : 'Open')
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))
