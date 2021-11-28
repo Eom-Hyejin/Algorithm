acfe37392139a9e11bf3a0936277d8494106c24b
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-28 18:46:08 +0900

    Add algorithm solving 21.11.28

--- November/211128.js
+++ November/211128.js
@@ -53,10 +53,37 @@ console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))
 
 
 function findEvenIndex(arr) {
-
+  let left = 0;
+  let right = 0;
+  
+  for(let i = 0; i < arr.length; i++){
+    if(i === 0){
+      right = arr.slice(1).reduce((acc, cur) => acc + cur, 0);
+      if(right === i){
+        return i;
+      }
+    } else{
+      left = arr.slice(0, i).reduce((acc, cur) => acc + cur, 0);
+      right = arr.slice(i+1).reduce((acc, cur) => acc + cur, 0);
+      if(left === right){
+        return i;
+      }
+    }
+  }
+  return -1;
 }
 
 console.log(findEvenIndex([1,2,3,4,3,2,1]))
 console.log(findEvenIndex([1,100,50,-51,1,1]))
 console.log(findEvenIndex([1,2,3,4,5,6]))
-console.log(findEvenIndex([20,10,30,10,10,15,35]))
+console.log(findEvenIndex([20,10,-80,10,10,15,35]))
+
+
+function centry(year) {
+  return Math.ceil(year/100);
+  // return year/100 === Math.floor(year/100) ? year/100 : Math.floor(year/100) + 1
+}
+
+console.log(centry(1705))
+console.log(centry(1900))
+console.log(centry(89))
