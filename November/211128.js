18f4ebc24d737d99c066674327bfd006cbdc9463
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-28 17:53:47 +0900

    Add algorithm solving 21.11.28

--- November/211128.js
+++ November/211128.js
@@ -49,3 +49,14 @@ function openOrSenior(data) {
 
 console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
 console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))
+
+
+
+function findEvenIndex(arr) {
+
+}
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]))
+console.log(findEvenIndex([1,100,50,-51,1,1]))
+console.log(findEvenIndex([1,2,3,4,5,6]))
+console.log(findEvenIndex([20,10,30,10,10,15,35]))
