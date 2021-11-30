bcaa65f2da86f73719994de58cc66ce775765797
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-01 04:09:11 +0900

    Add algorithm solving 21.12.01

--- December/211201.js
+++ December/211201.js
@@ -4,3 +4,32 @@ function oddOrEven(array) {
 
 console.log(oddOrEven([0, 1, 4]))
 console.log(oddOrEven([0, -1, -5]))
+
+
+
+function sortArray(array) {
+  // let arr = array.map((el) => [el])
+  // let oddNum = array.filter((el) => el % 2).sort((a, b) => a - b)
+  // let result = []
+
+  // for(let i = 0; i < arr.length; i++) {
+  //   if(arr[i][0] % 2) {
+  //     result.push([])
+  //   } else {
+  //     result.push(arr[i])
+  //   }
+  // }
+  // for(let i = 0; i < result.length; i++) {
+  //   if(result[i].length === 0) {
+  //     result[i] = oddNum[0]
+  //     oddNum.shift()
+  //   }
+  // }
+  // return result.flat()
+  const odd = array.filter((el) => el % 2).sort((a,b) => a - b);
+  return array.map((el) => el % 2 ? odd.shift() : el);
+}
+
+console.log(sortArray([7, 1]))
+console.log(sortArray([5, 3, 2, 8, 1, 4]))
+console.log(sortArray([5, 3, 1, 8, 0]))
