f793675da84e7541f0dcda875f1eb887b0a7c897
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-05 22:45:37 +0900

    Add algorithm solving 21.12.05

--- December/211205.js
+++ December/211205.js
@@ -1,1 +1,7 @@
+function getAverage (marks) {
+  return Math.floor(marks.reduce((acc, cur) => acc + cur, 0)/marks.length)
+}
 
+
+console.log(getAverage([1,2,3,4,5,]))
+console.log(getAverage([1,1,1,1,1,1,1,2]))
