9412134baff1c0407eeeb170cd43bf80e5317b90
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-05 23:34:12 +0900

    Add algorithm solving 21.12.05

--- December/211205.js
+++ December/211205.js
@@ -35,3 +35,26 @@ function countSmileys (arr) {
 console.log(countSmileys([]))
 console.log(countSmileys([ ':-)', ';~D', ':-D', ':_D' ]), 3)
 console.log(countSmileys([ ':---)', '))', ';~~D', ';D' ]), 1)
+
+
+
+
+function deleteNth (arr, n) {
+  // let result = []
+  // for(let i = 0; i < arr.length; i++) {
+  //   if(result.filter((el) => el === arr[i]).length < n) {
+  //     result.push(arr[i])
+  //   }
+  // }
+  // return result
+  
+  let obj = {}
+  return arr.filter((el) => {
+    obj[el] = obj[el] ? obj[el] + 1 : 1
+    return obj[el] <= n
+  })
+}
+
+
+console.log(deleteNth([20,37,20,21], 1))
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3))
