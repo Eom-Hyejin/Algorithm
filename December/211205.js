2c03f9f4749bc7b063281e4b4684970b74cdb16e
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-05 22:58:12 +0900

    Add algorithm solving 21.12.05

--- December/211205.js
+++ December/211205.js
@@ -5,3 +5,16 @@ function getAverage (marks) {
 
 console.log(getAverage([1,2,3,4,5,]))
 console.log(getAverage([1,1,1,1,1,1,1,2]))
+
+
+
+
+function reverseWords (str) {
+  return str.split(" ").map((el) => el.split("").reverse().join("")).join(" ")
+}
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+console.log(reverseWords('apple'))
+console.log(reverseWords('a b c d'))
+console.log(reverseWords('double  spaced  words'))
