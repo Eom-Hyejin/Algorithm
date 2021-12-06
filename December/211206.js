e717cbefc632bee69b3299dcdb175cbd5055b24b
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-06 23:19:53 +0900

    Add algorithm solving 21.12.06

--- December/211206.js
+++ December/211206.js
@@ -15,3 +15,27 @@ function inArray (array1, array2) {
 console.log(inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]))
 console.log(inArray(["live", "strong", "arp"], ["lively", "alive", "harp", "sharp", "armstrong"]))
 console.log(inArray(["tarp", "mice", "bull"], ["lively", "alive", "harp", "sharp", "armstrong"]))
+
+
+
+
+function longestConsec (strarr, k) {
+  if (k > strarr.length || k <= 0) return '';
+    
+  let longStr = '';
+  let newStr = '';
+  
+  for (let i = 0; i < strarr.length; i++){
+    newStr = strarr.slice(i, i+k);
+    if (newStr.join('').length > longStr.length ){
+      longStr = newStr.join('');
+    }
+  }
+  return longStr;
+}
+
+
+// console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+// console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15))
+// console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0))
+console.log(longestConsec([ 'wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu' ], 2))
