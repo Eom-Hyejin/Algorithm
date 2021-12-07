6f13a56831004e971186df027697878a719c3b01
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-07 23:30:22 +0900

    Add algorithm solving 21.12.07

--- December/211207.js
+++ December/211207.js
@@ -53,3 +53,24 @@ function maxSequence (arr) {
 console.log(maxSequence([]))
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 console.log(maxSequence([ -2, -1, -3, -4, -1, -2, -1, -5, -4 ]))
+
+
+
+
+function countPositivesSumNegatives (input) {
+  if (input === null || !input.length) {
+    return [0, 0]
+  }
+  let positive = 0
+  let negative = 0
+  for(let i = 0; i < input.length; i++) {
+    if(input[i] > 0) positive ++
+    else negative += input[i]
+  }
+  return [positive, negative]
+}
+
+
+console.log(countPositivesSumNegatives(null))
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
