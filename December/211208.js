0d501af1bb8819cd096380232e3882930978a6f6
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-08 22:45:29 +0900

    Add algorithm solving 21.12.08

--- December/211208.js
+++ December/211208.js
@@ -7,3 +7,22 @@ console.log(past(0, 1, 1))
 console.log(past(1, 1, 1))
 console.log(past(0, 0, 0))
 console.log(past(1, 0, 1))
+
+
+
+
+function expandedForm (num) {
+  let arr = []
+  num = String(num).split('')
+  for(let i = 0; i < num.length; i++) {
+    if(num[i] !== '0') {
+      arr.push(num[i] * Math.pow(10, num.length-1-i))
+    }
+  }
+  return arr.join(" + ")
+}
+
+
+console.log(expandedForm(12))
+console.log(expandedForm(42))
+console.log(expandedForm(70304))
