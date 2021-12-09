163d4a0c0ea30d4e294ad31e6f1f1104f8ba4a87
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-09 17:46:03 +0900

    Add algorithm solving 21.12.09

--- December/211209.js
+++ December/211209.js
@@ -9,3 +9,29 @@ console.log(removeSmallest([1, 2, 3, 4, 5]))
 console.log(removeSmallest([5, 3, 2, 1, 4]))
 console.log(removeSmallest([2, 2, 1, 2, 1]))
 console.log(removeSmallest([]))
+
+
+
+
+function formatDuration (seconds) {
+  let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+      result = [];
+
+  if (seconds === 0) return 'now';
+  
+  for (let key in time) {
+    if (seconds >= time[key]) {
+      let val = Math.floor(seconds/time[key]);
+      result.push(val += val > 1 ? ' ' + key + 's' : ' ' + key);
+      seconds %= time[key];
+    }
+  }
+  return result.length > 1 ? result.join(', ').replace(/,([^,]*)$/,' and'+'$1') : result[0]
+}
+
+
+console.log(formatDuration(1))
+console.log(formatDuration(62))
+console.log(formatDuration(120))
+console.log(formatDuration(3600))
+console.log(formatDuration(3662))
