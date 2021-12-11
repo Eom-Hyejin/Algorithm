36f84482906949312ff2b50f3950b189f39953f5
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-11 15:49:34 +0900

    Add algorithm solving 21.12.11

--- December/211211.js
+++ December/211211.js
@@ -1,15 +1,38 @@
 function count (string) {
-  let result = {}
-  for(let i = 0; i < string.length; i++) {
-    if(result[string[i]] === undefined) {
-      result[string[i]] = 1
-    } else {
-      result[string[i]]++
-    }
-  }
-  return result
+  // let result = {}
+  // for(let i = 0; i < string.length; i++) {
+  //   if(result[string[i]] === undefined) {
+  //     result[string[i]] = 1
+  //   } else {
+  //     result[string[i]]++
+  //   }
+  // }
+  // return result
+
+  // return string.split('').reduce((counts,char) => {
+  //   counts[char] = (counts[char]||0) + 1;
+  //   return counts;
+  // },{});
+
+  let count = {};
+  string.split('').forEach((el) => {
+    count[el] ? count[el]++ : count[el] = 1;
+  });
+  return count;
 }
 
 
 console.log(count('aba'))
 console.log(count(''))
+
+
+
+
+function bonusTime(salary, bonus) {
+  return bonus ? `£${salary}0` : `£${salary}`
+}
+
+
+console.log(bonusTime(10000, true))
+console.log(bonusTime(2, true))
+console.log(bonusTime(78, false))
