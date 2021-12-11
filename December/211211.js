84b383ca96c0c0b5ba48de883b8be6394cced699
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-11 16:03:18 +0900

    Add algorithm solving 21.12.11 [Object reduce]

--- December/211211.js
+++ December/211211.js
@@ -36,3 +36,27 @@ function bonusTime(salary, bonus) {
 console.log(bonusTime(10000, true))
 console.log(bonusTime(2, true))
 console.log(bonusTime(78, false))
+
+
+
+
+function cakes (recipe, available) {
+  // let recipeKey = Object.keys(recipe)
+  // let availableKey = Object.keys(available)
+  // if(!availableKey.includes(...recipeKey)) {
+  //   return 0
+  // }
+  // let result = []
+  // for(let i = 0; i < recipeKey.length; i++) {
+  //   result.push(Math.floor(available[recipeKey[i]] / recipe[recipeKey[i]]))
+  // }
+  // return Math.min(...result)
+  
+  return Math.min(
+    ...Object.keys(recipe).map(key => Math.floor(available[key] / recipe[key] || 0))
+  )
+}
+
+
+console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
