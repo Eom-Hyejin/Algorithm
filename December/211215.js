ed35ffa2a6ec2859c2d1e0448e2b18450b07f9c1
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-15 23:57:27 +0900

    Add algorithm solving 21.12.15

--- December/211215.js
+++ December/211215.js
@@ -1,1 +1,25 @@
+function grow(x) {
+  return x.reduce((acc, cur) => acc * cur)
+}
 
+
+console.log(grow([1, 2, 3]))
+console.log(grow([4, 1, 1, 1, 4]))
+console.log(grow([2, 2, 2, 2, 2, 2]))
+
+
+
+
+function orderWeight (string) {
+  let weightNum = string.split(" ").map((el) => [el.split("").reduce((acc, cur) => Number(acc) + Number(cur), 0)])
+  let num = string.split(" ")
+  for(let i = 0; i < weightNum.length; i++) {
+    weightNum[i][0] = num[i]
+  }
+  console.log(string)  
+  console.log(weightNum)  
+}
+
+
+console.log(orderWeight("103 123 4444 99 2000"))
+console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))
