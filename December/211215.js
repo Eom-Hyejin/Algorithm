0318ca69c473f0a37beb3ecd7b57bf0c9f7f3076
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-16 00:18:32 +0900

    Add algorithm solving 21.12.15

--- December/211215.js
+++ December/211215.js
@@ -10,24 +10,32 @@ console.log(grow([2, 2, 2, 2, 2, 2]))
 
 
 
-function orderWeight (strng) {
-  let weightNum = strng.split(" ").map((el) => [el.split("").reduce((acc, cur) => Number(acc) + Number(cur), 0)])
-  let num = strng.split(" ")
-  for(let i = 0; i < weightNum.length; i++) {
-    weightNum[i][1] = num[i]
-  }
-  let result = []
-  let useNum = []
-  weightNum.sort((a, b) => a[0] - b[0])
-  for(let i = 0; i < weightNum.length; i++) {
-    if(!useNum.includes(weightNum[i][0])) {
-      useNum.push(weightNum[i][0])
-      result.push([weightNum[i][1]])
-    } else {
-      result[result.length-1].push(weightNum[i][1])
-    }
-  } 
-  return result.map((el) => el.sort()).join(" ").replace(/,/g, " ")
+// function orderWeight (strng) {
+//   let weightNum = strng.split(" ").map((el) => [el.split("").reduce((acc, cur) => Number(acc) + Number(cur), 0)])
+//   let num = strng.split(" ")
+//   for(let i = 0; i < weightNum.length; i++) {
+//     weightNum[i][1] = num[i]
+//   }
+//   let result = []
+//   let useNum = []
+//   weightNum.sort((a, b) => a[0] - b[0])
+//   for(let i = 0; i < weightNum.length; i++) {
+//     if(!useNum.includes(weightNum[i][0])) {
+//       useNum.push(weightNum[i][0])
+//       result.push([weightNum[i][1]])
+//     } else {
+//       result[result.length-1].push(weightNum[i][1])
+//     }
+//   } 
+//   return result.map((el) => el.sort()).join(" ").replace(/,/g, " ")
+// }
+
+function digitSum(str) {
+  return str.split('').reduce((s, e) => s + parseInt(e), 0)
+}
+
+function orderWeight(str) {
+  return str.split(' ').sort((a, b) => digitSum(a) - digitSum(b) || a.localeCompare(b)).join(' ');
 }
 
 
