739e89bbc4a6e0ef692764dc737cd33813df7ece
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-16 00:15:25 +0900

    Add algorithm solving 21.12.15

--- December/211215.js
+++ December/211215.js
@@ -10,17 +10,26 @@ console.log(grow([2, 2, 2, 2, 2, 2]))
 
 
 
-function orderWeight (string) {
-  let weightNum = string.split(" ").map((el) => [el.split("").reduce((acc, cur) => Number(acc) + Number(cur), 0)])
-  let num = string.split(" ")
+function orderWeight (strng) {
+  let weightNum = strng.split(" ").map((el) => [el.split("").reduce((acc, cur) => Number(acc) + Number(cur), 0)])
+  let num = strng.split(" ")
   for(let i = 0; i < weightNum.length; i++) {
     weightNum[i][1] = num[i]
   }
   let result = []
-  weightNum.sort((a, b) => b[0] - a[0])
-  console.log(weightNum)  
+  let useNum = []
+  weightNum.sort((a, b) => a[0] - b[0])
+  for(let i = 0; i < weightNum.length; i++) {
+    if(!useNum.includes(weightNum[i][0])) {
+      useNum.push(weightNum[i][0])
+      result.push([weightNum[i][1]])
+    } else {
+      result[result.length-1].push(weightNum[i][1])
+    }
+  } 
+  return result.map((el) => el.sort()).join(" ").replace(/,/g, " ")
 }
 
 
 console.log(orderWeight("103 123 4444 99 2000"))
-console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))
+console.log(orderWeight("2000 1234000 44444444 9999 11 11 22 123 10003"))
