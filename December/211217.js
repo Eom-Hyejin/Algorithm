6e15ec4f66fa0d985fa7f0e910580f25586b4346
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-17 03:54:13 +0900

    Add algorithm solving 21.12.17

--- December/211217.js
+++ December/211217.js
@@ -6,3 +6,35 @@ function points (games) {
 
 console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))
 console.log(points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4']))
+
+
+
+
+function sumDigPow (a, b) {
+  // let result = []
+  // for(let i = a; i <= b; i++) {
+  //   let sum = 0
+  //   for(let j = 0; j < String(i).length; j++) {
+  //     sum += Math.pow(String(i)[j], j+1)
+  //     if(i === sum) {
+  //       result.push(i)
+  //     }
+  //   }
+  // }
+  // return result
+  
+  let result = [];
+  while(a <= b){
+    if(a.toString().split('').reduce((x,y,i)=>x + y ** (i + 1),0) == a)
+      result.push(a);
+    a++;
+  }
+  return result;
+}
+
+
+console.log(sumDigPow(1, 10))
+console.log(sumDigPow(24, 40))
+console.log(sumDigPow(1, 100))
+console.log(sumDigPow(10, 100))
+console.log(sumDigPow(90, 100))
