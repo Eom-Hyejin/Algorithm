23f6d03e597f7bfedf0520d56259995894a23b02
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-20 00:19:26 +0900

    Add algorithm solving 21.12.20

--- December/211220.js
+++ December/211220.js
@@ -12,3 +12,20 @@ function twoSum (numbers, target) {
 console.log(twoSum([1, 2, 3], 4))
 console.log(twoSum([1234,5678,9012], 14690))
 console.log(twoSum([2, 2, 3], 4))
+
+
+
+
+function stray (numbers) {
+  // for(let i = 0; i < numbers.length; i++) {
+  //   if(numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+  //     return numbers[i]
+  //   }
+  // }
+  
+  return numbers.reduce((acc, cur) => acc ^ cur)
+}
+
+
+console.log(stray([1, 1, 2]))
+console.log(stray([17, 17, 3, 17, 17, 17, 17]))
