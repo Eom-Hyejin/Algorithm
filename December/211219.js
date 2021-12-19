1ba2bffe9ae504d1bd5400bd9eecc3a9355166ba
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-19 23:44:32 +0900

    Add algorithm solving 21.12.19

--- December/211219.js
+++ December/211219.js
@@ -1,1 +1,18 @@
+function solution (number) {
+  let result = ""
+  let num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
+  num.map((el, idx) => {
+    while(number >= el) {
+      result += roman[idx]
+      number -= el
+    }
+  })
+  return result
+}
+
+console.log(solution(1))
+console.log(solution(2))
+console.log(solution(10))
+console.log(solution(2008))
