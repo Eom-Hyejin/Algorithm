9bbca5a18000fde9535e22efbe86a111110f0dc5
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-24 01:09:21 +0900

    Add algorithm solving 21.12.24

--- December/211224.js
+++ December/211224.js
@@ -16,3 +16,39 @@ console.log(change124(2))
 console.log(change124(3))
 console.log(change124(4))
 
+
+
+
+// function solution (arr) {
+//   let result = []
+//   while(arr.length > 0) {
+//     let pop = arr.shift()
+//     if(result.length === 0) {
+//       result.push(lcm(pop, arr[0]))
+//     } else {
+//       let shift = result.shift()
+//       result.push(lcm(pop, shift))
+//     }
+//   }
+//   return result[0]
+// }
+
+// function lcm(a, b) {
+//   return a * b / gcd(a, b)
+// }
+
+// function gcd(a, b) {
+//   return a % b === 0 ? b : gcd(b, a % b)
+// }
+
+function solution (num) {
+  return num.reduce((a, b) => a * b / gcd(a, b))
+}
+
+function gcd (a, b) {
+  return a % b === 0 ? b : gcd(b, a % b)
+}
+
+console.log(solution([2, 6, 8, 14]))
+console.log(solution([1, 2, 3]))
+
