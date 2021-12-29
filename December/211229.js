b6ebd766a6bbefa5d1db89b4c1d39d1c3e3da745
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-29 12:03:18 +0900

    Add algorithm solving 21.12.29

--- December/211229.js
+++ December/211229.js
@@ -29,7 +29,7 @@ function sortFile (files) {
       }
     }
   })
-  
+
   return result.map((el) => el.value.join(""))
 }
 
@@ -38,3 +38,28 @@ console.log(sortFile(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.
 ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"])
 console.log(sortFile(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]),
 ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"])
+
+
+
+
+function makeMaxNum (number, k) {
+  let q = []
+
+  for(let i = 0; i < number.length; i++) {
+    const el = number[i]
+
+    while(k > 0 && q[q.length-1] < el) {
+      q.pop()
+      k--
+    }
+    q.push(el)
+  }
+  
+  q.splice(q.length-k, k)
+  return q.join("")
+}
+
+
+console.log(makeMaxNum("1924", 2))
+console.log(makeMaxNum("1231234", 3))
+console.log(makeMaxNum("4177252841", 4))
