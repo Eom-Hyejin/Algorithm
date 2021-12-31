963e91d85648e26834f7918f79298a4ed0222846
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-31 20:25:23 +0900

    Add algorithm solving 21.12.31

--- December/211231.js
+++ December/211231.js
@@ -26,3 +26,25 @@ console.log(findSquare([[0,1,1,1]
                       ,[0,0,1,0]]), 9)
 console.log(findSquare([[0,0,1,1],
                         [1,1,1,1]]), 4)
+
+
+
+
+function makeMax (numbers) {
+  // const numbersString = numbers.map((num) => String(num));
+  // numbersString.sort((a, b) => {
+  //   return parseInt(b + a) - parseInt(a + b);
+  // })
+  // const answer = numbersString.join('');
+  // return answer[0] === '0' ? '0' : answer
+  
+  let answer = numbers.map(v=>v+'')
+  .sort((a,b) => (b+a)*1 - (a+b)*1)
+  .join('');
+
+  return answer[0]==='0'?'0':answer;
+}
+
+
+console.log(makeMax([6, 10, 2]))
+console.log(makeMax([3, 30, 34, 5, 9]))
