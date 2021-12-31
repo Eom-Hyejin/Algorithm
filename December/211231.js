04c570f3cbf93e1f57fe04e67ccb8cd3537d81de
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-12-31 20:41:52 +0900

    Add algorithm solving 21.12.31

--- December/211231.js
+++ December/211231.js
@@ -37,7 +37,7 @@ function makeMax (numbers) {
   // })
   // const answer = numbersString.join('');
   // return answer[0] === '0' ? '0' : answer
-  
+
   let answer = numbers.map(v=>v+'')
   .sort((a,b) => (b+a)*1 - (a+b)*1)
   .join('');
@@ -48,3 +48,50 @@ function makeMax (numbers) {
 
 console.log(makeMax([6, 10, 2]))
 console.log(makeMax([3, 30, 34, 5, 9]))
+
+
+
+
+function isSubsetOf (base, sample) {
+  // const long = base.sort((a, b) => a - b)
+  // const short = sample.sort((a, b) => a - b)
+
+  // if(short[0] > long[long.length-1] || long[0] > short[short.length - 1]) {
+  //   return false
+  // }
+  // let result
+
+  // for(let i = 0; i < short.length; i++) {
+  //   for(let j = 0; j < long.length; j++) {
+  //     if(short[i] === long[j]) {
+  //       result = true
+  //       break;
+  //     }
+  //     result = false
+  //   }
+  // }
+  // return result
+
+  base = base.sort((a, b) => a - b)
+  sample = sample.sort((a, b) => a - b)
+
+  const findArr = (el, arr, from) => {
+    for(let i = from; i < arr.length; i++) {
+      if(arr[i] === el) return i
+      // else if (el < arr[i]) return -1
+    }
+    return -1
+  }
+
+  let baseIdx = 0
+  for(let i = 0; i < sample.length; i++) {
+    baseIdx = findArr(sample[i], base, baseIdx)
+    if(baseIdx === -1) return false
+  }
+  return true
+}
+
+
+console.log(isSubsetOf([1, 2, 3, 4, 5], [1, 3]))
+console.log(isSubsetOf([1, 2, 3, 4, 5], [6, 7]))
+console.log(isSubsetOf([10, 99, 123, 7], [11, 100, 99, 123]))
