8552dce89c529234d4e7c11aa554b8d6e60587b8
Eom-Hyejin <qnwkdhk9192@gmail.com>
2022-01-03 18:04:29 +0900

    Add algorithm solving 22.01.03

--- January/220103.js
+++ January/220103.js
@@ -66,3 +66,44 @@ function rotatedArraySearch (rotated, target) {
 
 console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2))
 console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100))
+
+
+
+
+function primePassword (curPwd, newPwd) {
+  function isPrime (num) {
+    let sqrt = Math.sqrt(num)
+    if(num % 2 === 0) return false
+    for(let i = 3; i <= sqrt; i+=2) {
+      if(num % i === 0) return false
+    }
+    return true
+  }
+
+  let visited = []
+  let next = [[curPwd, 0]]
+
+  while(next.length) {
+    let [now, count] = next.shift()
+    if(now === newPwd) return count
+
+    for(let i = 0; i < 4; i++) {
+      let arr = [...`${now}`].map(e => Number(e))
+      for(let j = 0; j <= 9; j++) {
+        if(arr[i] !== j) {
+          arr[i] = j
+          let made = Number(arr.join(''))
+          if(made === newPwd) return count+1;
+          if(isPrime(made) && made > 1000 && !visited.includes(made)) {
+            visited.push(made)
+            next.push([made, count+1])
+          }
+        }
+      }
+    }
+  }
+}
+
+
+console.log(primePassword(1033, 1033))
+console.log(primePassword(3733, 8779))
